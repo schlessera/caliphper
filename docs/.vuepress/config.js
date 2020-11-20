c14168475bb9ad8ea97d3c17b9c54e8a2e0e03fb
Alain Schlesser <alain.schlesser@gmail.com>
2020-11-20 07:39:04 +0100

    Initial commit to test docs generation

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'CaliPHPer',
+  description: 'Benchmarking tool for PHP',
+  base: '/caliphper/'
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+title: CaliPHPer Documentation
+lang: en-US
+---
+# CaliPHPer
+
+Benchmarking tool for PHP
